5f24a55576dac859c515fa7e56c1aeb1c32e867c
faturalfi87 <faturalfi87@gmail.com>
2023-03-14 16:41:07 +0700

    Menambah file

--- Tugas Modul 01.py
+++ Tugas Modul 01.py
@@ -1,2 +1,7 @@
 print ("Khalifahtur Alfi")
 print ("I0322065")
+
+#Cerita tempat tinggal dan visualisasi kampung halaman
+print ("Saya berasal dari kota jambi. Pada awalnya tempat tinggal saya berada di Kabupaten Muaro Jambi, tepatnya di Kelurahan Mendalo Darat. Pada saat itu, saya tinggal disana selama kurang lebih 19 tahun, hingga pada akhirnya saya dan keluarga pindah ke kota.")
+print ("Tempat tinggal yang saya tempati sekarang berada di Kecamatan Kota Baru, Kelurahan The Hok, dan beralamatkan Jalan Raden Wijaya, Lorong Akimar, Komplek Akydo, Kebon Kopi dan dekat dengan Rumah Sakit yang hanya berjarak kurang lebih 500 meter. ")
+print ("Tempat tinggal saya saat ini berada di daerah yang dekat pusat Kota Jambi, dan juga daerah saya terkenal dengan pusat jajanan street food di Kota Jambi. Selain itu daerah saya ini yaitu Kota baru sebutannya, banyak juga terdapat perkantoran kedinasan Kota Jambi, beberapa Universitas Swasta di Jambi, Politeknik Kesehatan, Taman, pusat tempat bermainan untuk anak-anak kecil dan pusat tempat nongkrong para remaja.")
